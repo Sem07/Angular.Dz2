aeee3bc272861ab6a836258f9c5ba9c9a27bc2a8
Sem07 <Sem007@live.com>
2020-09-10 17:11:35 +0300

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDolistComponent } from './Components/to-dolist/to-dolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDolistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
